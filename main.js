19de7f99a4af85e1d04a1cb4393498c4ea401523
babyoilrig <catrin.mentzoni@yahoo.com>
2023-02-16 11:08:32 +0000

    Working - sourcing the Pokemon info

--- main.js
+++ main.js
@@ -1,15 +1,14 @@
 let pokeImage = document.querySelector("#pokemonPic")
 let pokeName = document.querySelector("#pokeName")
 let pokedexNo = document.querySelector("#pokedexNo")
-let pokeType = document.querySelector("#pokeType")
 let pokeHeight = document.querySelector("#pokeHeight")
 let pokeWeight = document.querySelector("#pokeWeight")
-let pokeHp = document.querySelector("#pokeHp")
+let pokeSpeed = document.querySelector("#pokeSpeed")
 let pokeAttack = document.querySelector("#pokeAttack")
 let pokeDefense = document.querySelector("#pokeDefense")
-let pokeSpecialAttack=document.querySelector("#pokeSpecial-attack")
-let pokeSpecialDefense=document.querySelector("#pokeSpecial-defense")
-let pokeSpeed=document.querySelector("#pokeSpeed")
+// let pokeSpecialAttack=document.querySelector("#pokeSpecial-attack")
+// let pokeSpecialDefense=document.querySelector("#pokeSpecial-defense")
+
 
 
         
@@ -21,15 +20,14 @@ let pokeSpeed=document.querySelector("#pokeSpeed")
         console.log(pokemon.name, pokemon.id, pokemon.types[0].type.name, pokemon.sprites.other.dream_world.front_default);
         pokeImage.src = pokemon.sprites.other.dream_world.front_default;
         pokeName.innerHTML = pokemon.name;
-        pokeType.innerHTML =  pokemon.types[0].type.name;
         pokedexNo.innerHTML = "pokedex No: " + pokemon.id;
         pokeHeight.innerHTML = "Height: " + pokemon.height;
         pokeWeight.innerHTML = "Weight: " + pokemon.weight;
-        pokeHp.innerHTML = "Hp: " + pokemon.stats[0].base_stat;
+        pokeSpeed.innerHTML = "Speed: " + pokemon.stats[5].base_stat;
         pokeAttack.innerHTML = "Attack: " + pokemon.stats[1].base_stat;
         pokeDefense.innerHTML = "Defense: " + pokemon.stats[2].base_stat;
-        pokeSpecialAttack.innerHTML = "Special Attack: " + pokemon.stats[3].base_stat;
-        pokeSpecialDefense.innerHTML = "Special Defense: " + pokemon.stats[4].base_stat;
-        pokeSpeed.innerHTML = "Speed: " + pokemon.stats[5].base_stat;
+        // pokeSpecialAttack.innerHTML = "Special Attack: " + pokemon.stats[3].base_stat;
+        // pokeSpecialDefense.innerHTML = "Special Defense: " + pokemon.stats[4].base_stat;
+        
 
     };
